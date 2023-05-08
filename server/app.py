cf7f4d87df191af35b15fccf3b06f6dd06936981
Aastha <saxena.aastha91@gmail.com>
2023-05-08 09:50:16 -0400

    ok

--- server/app.py
+++ server/app.py
@@ -19,15 +19,39 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id==id).first()
+    response_body = f''
+    response_body += f'<ul><h2>ID: {animal.id}</h2></ul>'
+    response_body += f'<ul><h2>Name: {animal.name}</h2></ul>'
+    response_body += f'<ul><h2>Species: {animal.species}</h2></ul>'
+    response_body += f'<ul><h2>Zookeeper: {animal.zookeeper}</h2></ul>'
+    response_body += f'<ul><h2>Enclosure: {animal.enclosure}</h2></ul>'
+    
+    return make_response(response_body,200)
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Animal.id==id).first()
+    response_body = f''
+    response_body += f'<ul><h2>ID: {zookeeper.id}</h2></ul>'
+    response_body += f'<ul><h2>Name: {zookeeper.name}</h2></ul>'
+    response_body += f'<ul><h2>Species: {zookeeper.birthday}</h2></ul>'
+    for animal in zookeeper.animals:
+        response_body += f'<ul><h2>Animal: {animal.name}</h2></ul>'
+
+    return make_response(response_body,200)
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id==id).first()
+    response_body = f''
+    response_body += f'<ul><h2>ID: {enclosure.id}</h2></ul>'
+    response_body += f'<ul><h2>Environment: {enclosure.environment}</h2></ul>'
+    response_body += f'<ul><h2>Open to Visitors: {enclosure.open_to_visitors}</h2></ul>'
+    for animal in enclosure.animals:
+        response_body += f'<ul><h2>Animal: {animal.name}</h2></ul>'
+
+    return make_response(response_body,200)
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from random import choice as rc
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -9,15 +10,35 @@ db = SQLAlchemy(metadata=metadata)
 
 class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
-
+    
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    birthday = db.Column(db.String)
+
+    #relationships
+    animals = db.relationship('Animal',back_populates='zookeeper')
+
+    
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean)
+
+    #relationships
+    animals = db.relationship('Animal',back_populates='enclosure')
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosuer_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+    #relationships
+    zookeeper = db.relationship('Zookeeper', back_populates='animals')
+    enclosure = db.relationship('Enclosure', back_populates='animals')
+
